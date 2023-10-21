089aee893fb4a7d7d5b79c1524456c619dd7c63d
Chris Engelhard <c.engelhard@digi4care.nl>
2023-10-21 15:53:02 +0200

    update packages

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-    plugins: {
-        'postcss-nesting': {
-            /* plugin options */
-        },
-        '@csstools/postcss-global-data': {
-            files: ['src/lib/styles/custom-media-queries.css']
-        },
-        'postcss-preset-env': {
-            stage: 3,
-            features: {
-                'nesting-rules': true,
-                'custom-media-queries': true,
-                'media-query-ranges': true
-            }
-        },
-        autoprefixer: { grid: 'autoplace', flexbox: 'no-2009' },
-        'postcss-csso': {
-            /* plugin options */
-        }
-    }
+	plugins: {
+		'postcss-nesting': {
+			/* plugin options */
+		},
+		'@csstools/postcss-global-data': {
+			files: ['src/lib/styles/custom-media-queries.css']
+		},
+		'postcss-preset-env': {
+			stage: 3,
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true,
+				'media-query-ranges': true
+			}
+		},
+		autoprefixer: { grid: 'autoplace', flexbox: 'no-2009' },
+		'postcss-csso': {
+			/* plugin options */
+		}
+	}
 };
