d593087f5952bc959492270736b9b4463edae49a
Chris Engelhard <c.engelhard@digi4care.nl>
2023-10-21 16:37:35 +0200

    Tailwindcss ready

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'tailwindcss/nesting': 'postcss-nesting',
+		tailwindcss: {},
+		autoprefixer: { grid: 'autoplace', flexbox: 'no-2009' }
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-    import Test from "@/components/Test.svelte";
+	import Test from '@/components/Test.svelte';
 </script>
 
+<div class="container bg-magnum-966 text-white">
+	<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+	<p class="font-mono">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<Test />
+</div>
 
-<Test />
+<style lang="postcss">
+	.container {
+		box-shadow: 0px 1px 10px theme('colors.magnum.950');
+	}
+</style>
