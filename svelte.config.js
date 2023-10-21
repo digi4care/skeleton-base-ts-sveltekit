b32cf66b620420a4c861b6e26bc9fa82b0d368c5
Chris Engelhard <c.engelhard@digi4care.nl>
2023-10-21 15:57:07 +0200

    updates packages

--- svelte.config.js
+++ svelte.config.js
@@ -3,27 +3,26 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-    kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter(),
-        alias: {
-            '@': 'src/lib',
-            '@/*': 'src/lib/*'
-        },
-        files: {
-            hooks: {
-                client: 'src/hooks/client.ts',
-                server: 'src/hooks/server.ts'
-            }
-        }
-
-    }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			'@': 'src/lib',
+			'@/*': 'src/lib/*'
+		},
+		files: {
+			hooks: {
+				client: 'src/hooks/client.ts',
+				server: 'src/hooks/server.ts'
+			}
+		}
+	}
 };
 
 export default config;
