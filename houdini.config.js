f12e1b0a1f13e3b5c9752e65a8ef46e97eda3189
digi4care <c.engelhard@digi4care.nl>
2024-02-01 18:02:59 +0100

    Installed Houdini GraphQL

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://webshop.voorbeeld.link/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
