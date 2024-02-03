a8bccdb12c8a683b5e7505598a2401f75ddd214d
Chris Engelhard <c.engelhard@digi4care.nl>
2024-02-03 18:10:19 +0100

    show ProductCategoriesList

--- src/routes/(app)/winkel/+page.svelte
+++ src/routes/(app)/winkel/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import type { GetShopCategories$result } from '$houdini';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-    // Destructure
+	// Destructure
 	let { GetProducts, GetShopCategories, GetShopColors } = data;
 	$: ({ GetProducts, GetShopCategories, GetShopColors } = data);
 
-	// $: console.log($GetShopCategories.data?.productCategories?.edges);
+	$: console.log($GetShopCategories.data?.productCategories?.edges);
 	// $: console.log($GetShopColors.data?.allPaColor?.edges);
 	// $: console.log($GetProducts.data?.products?.nodes);
+
+	import ProductCategoriesList from '@/lib/components/shop/ProductCategoriesList.svelte';
+	const categories = $GetShopCategories.data?.productCategories?.edges;
 </script>
+
+<ProductCategoriesList {categories} />
