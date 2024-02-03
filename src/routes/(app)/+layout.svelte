70f733a20114c3cf3080ef3df3baca5568d4eb9f
Chris Engelhard <c.engelhard@digi4care.nl>
2024-02-03 16:18:28 +0100

    bump

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -20,7 +20,7 @@
 
 	// $: console.log((data.posts.status === 200 && data.posts.data.footer));
 	// TODO: We need SEO ...
-	console.log(data.data.headerMenu?.menuItems?.nodes)
+	// console.log(data.data.headerMenu?.menuItems?.nodes)
 </script>
 
 <svelte:head>
