5e40940cd7a82a800f70ee2be2d4819d42198b10
Chris Engelhard <c.engelhard@digi4care.nl>
2024-02-03 18:09:58 +0100

    Firest commit

---
+++ src/lib/components/shop/ProductCategoriesList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { GetShopCategories$result } from '$houdini';
+	import type { ShopCategoryEdgeType } from '@/types';
+
+	// Deze types kunnen nu gebruikt worden voor het definiëren van je data structuren
+	export let categories: ShopCategoryEdgeType[] | undefined;
+</script>
+
+<ul class="product__categories">
+	{#if categories && categories.length > 0}
+		{#each categories as category (category.node.id)}
+			<li class="product__category">
+				<a href="/product-category/{category.node.slug || ''}" title=""
+					>{category.node.name || ''}</a
+				>
+			</li>
+		{/each}
+	{/if}
+</ul>
