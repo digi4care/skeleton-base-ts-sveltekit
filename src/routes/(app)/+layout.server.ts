42d366a80f265cdd6479c7de02d543a2a8a65cec
digi4care <c.engelhard@digi4care.nl>
2024-02-03 11:03:19 +0100

    fixed typo too types

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,8 +1,9 @@
 import { GetLayoutStore } from '$houdini';
-import type LayoutServerLoad from './types'
+import type { ServerLoadEvent} from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
 
 /* @type { import('./$houdini').LayoutServerLoad } */
-export const load: LayoutServerLoad = async (event) => {
+export const load: LayoutServerLoad = async (event: ServerLoadEvent) => {
 	const GetLayout = new GetLayoutStore();
 	const { data, errors } = await GetLayout.fetch({ event });
 
