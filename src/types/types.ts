2cf97f932bf8b57d90968f3a06385b168792f162
Chris Engelhard <c.engelhard@digi4care.nl>
2024-02-05 15:12:57 +0100

    More types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,12 @@ export type NonNullableEdges<T> =
 	NonNullable<T> extends { edges: infer E } ? (E extends Array<infer U> ? U : never) : never;
 export type NonNullableNodes<T> =
 	NonNullable<T> extends { nodes: infer N } ? (N extends Array<infer V> ? V : never) : never;
+
+export type Maybe<T> = T | null;
+export type Nullable<T> = T | undefined;
+export type NullableArray<T> = T[] | undefined;
+export type NullableObject<T> = T | undefined;
+export type NullableString = string | undefined;
+export type NullableNumber = number | undefined;
+export type NullableBoolean = boolean | undefined;
+export type NullableDate = Date | undefined;
