7a9723cc9987035237d648d80ffe04519fb7d637
Chris Engelhard <c.engelhard@digi4care.nl>
2024-02-14 09:19:53 +0100

    added to recipies

---
+++ src/lib/components/shop/recipies/products/ProductCard.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import type { MyProductTaxonomies, MyProductContentSmall } from '$houdini';
+	import { fragment, graphql } from '$houdini';
+
+	export let product: any;
+	export let priority: boolean;
+	export let className: string;
+
+	import * as C from '@/lib/shadcn/ui/card';
+	import { cn } from '@/lib/shadcn/utils/ui';
+
+	import ImageWrapper from './ImageWrapper.svelte';
+
+	let product_tax = product as unknown as MyProductTaxonomies;
+	let product_content = product as unknown as MyProductContentSmall;
+
+	$: productTax = fragment(
+		product_tax,
+		graphql(`
+			fragment MyProductTaxonomies on Product {
+				productCategories(first: 20, where: {hideEmpty: true}) {
+					nodes {
+						id
+						slug
+						name
+						count
+					}
+				}
+				productTags(first: 20, where: {hideEmpty: true}) {
+					nodes {
+						id
+						slug
+						name
+						count
+					}
+				}
+			}
+		`)
+	);
+	$: productContentSmall = fragment(
+		product_content,
+		graphql(`
+			fragment MyProductContentSmall on ProductUnion {
+				id
+				databaseId
+				slug
+				name
+				type
+				... on ProductUnion {
+					shortDescription(format: RAW)
+				}
+				image {
+					...ThumbnailImageFields @mask_disable
+				}
+				... on ProductWithPricing {
+					price
+					regularPrice
+					salePrice
+				}
+				... on InventoriedProduct {
+					stockStatus
+					stockQuantity
+					soldIndividually
+				}
+			}
+		`)
+	);
+	$: categories = $productTax.productCategories?.nodes;
+	$: tags = $productTax.productTags?.nodes;
+	// $: console.log($productTax.productCategories?.nodes);
+</script>
+
+<C.Root class={cn(className, 'flex flex-col')}>
+	<C.Header class="p-4">
+		<C.Title class="w-full truncate whitespace-nowrap font-serif text-base"
+			>{$productContentSmall.name}</C.Title
+		>
+		<ImageWrapper source={$productContentSmall?.image} {priority} />
+	</C.Header>
+	<C.Content class="flex flex-col space-y-2 p-4">
+		<p class="font-serif text-sm font-semibold">{@html $productContentSmall.price}</p>
+	</C.Content>
+	<C.Footer class="mt-auto flex-col p-4">
+		{#if categories}
+			<div class="product__categories">
+				<ul class="justify center flex gap-x-4 flex-wrap">
+					{#each categories as cat, index (cat.id)}
+						<li>
+							<a href={`/product-category/${cat.slug}`} title={cat.name}>{cat.name} (<strong>{cat.count}</strong>)</a
+							>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/if}
+		{#if tags}
+			<div class="product__tags">
+				<ul class="justify center flex gap-x-4 flex-wrap">
+					{#each tags as tag, index (tag.id)}
+						<li>
+							<a href={`/product-tag/${tag.slug}`} title={tag.name}>{tag.name} (<strong>{tag.count}</strong>)</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/if}
+		<button class="add_to_cart">Toevoegen</button>
+	</C.Footer>
+</C.Root>
